9e6ed88172e2ca9214f4ab16fa609eca2d4d6a67
Jack Chen <32407740+JackyC415@users.noreply.github.com>
2020-03-27 23:43:11 -0700

    Update SponsorServiceImpl.java

--- lab2/src/main/java/com/cmpe275/lab2/service/SponsorServiceImpl.java
+++ lab2/src/main/java/com/cmpe275/lab2/service/SponsorServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cmpe275.lab2.dao.SponsorRepository;
+import com.cmpe275.lab2.errors.AlreadyExistsException;
+import com.cmpe275.lab2.errors.NotFoundException;
 import com.cmpe275.lab2.model.Sponsor;
 
 /*
@@ -14,29 +16,54 @@ public class SponsorServiceImpl implements SponsorService {
 
 	@Autowired
 	private SponsorRepository sponsorDao;
-	
+
 	@Override
-	public Sponsor createSponsor( Sponsor sponsor) {
-		return null;
+	public Sponsor createSponsor(Sponsor sponsor) {
+		try {
+			Sponsor existingSponsor = sponsorDao.findByName(sponsor.getName());
+			if (existingSponsor != null) {
+				throw new AlreadyExistsException("Sponsor with given Id already present!");
+			} else {
+				Sponsor newSponsor = new Sponsor(sponsor.getName(), sponsor.getDescription(), sponsor.getAddress());
+				return sponsorDao.save(newSponsor);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e.fillInStackTrace());
+		}
 	}
 
 	@Override
 	public Sponsor updateSponsor(Sponsor sponsor) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Sponsor getSponsor(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Sponsor sponsor = sponsorDao.findByName(name);
+			if (sponsor == null) {
+				throw new NotFoundException("Sponsor does not exist!");
+			} else {
+				return sponsor;
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e.fillInStackTrace());
+		}
 	}
 
 	@Override
 	public Sponsor deleteSponsor(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Sponsor sponsorName = sponsorDao.findByName(name);
+			if (sponsorName == null) {
+				throw new NotFoundException("Sponsor does not exist!");
+			} else {
+				// return sponsorDao.deleteById(name);
+				return null;
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e.fillInStackTrace());
+		}
 	}
 
-	
 }
